1b9348966bf523fe98e7a3e424774ef909d88c76
Emmanuel <oketunbi.olufunke@gmail.com>
2023-06-01 12:06:57 +0100

    linked the css and js file to the html and added some basic  javaScript code

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector(".header").addEventListener("click",function(){
+    alert("this is a header")
+    console.log("header added")
+    document.write("header has been added")
+})
