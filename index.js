13f2ce2fc2aa0db820a0b627b03d357dde218b6e
Emmanuel <oketunbi.olufunke@gmail.com>
2023-06-01 12:59:25 +0100

    added some styling to the second header and removed a line in index.js

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ console.log("hello git")
 document.querySelector(".header").addEventListener("click",function(){
     alert("this is a header")
     console.log("header added")
-    document.write("header has been added")
 })
